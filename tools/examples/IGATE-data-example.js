5f82f9837b8ef2e0d0bf9f82f868666ed15f3cb1
Max Brito <brito@triplecheck.tech>
2025-09-28 13:21:22 +0200

    Added loading of igates, still incomplete but showing

---
+++ tools/examples/IGATE-data-example.js
@@ -0,0 +1,58 @@
+window.IGATE = 
+[
+  {
+    "latencyMs": 0,
+    "packetsPerHour": 0,
+    "errorCount": 0,
+    "antennaHeightM": 0.0,
+    "powerWatts": 0.0,
+    "digipeater": false,
+    "messagingEnabled": false,
+    "type": "IGATE",
+    "callsign": "W1QZ-9",
+    "coordinates": {
+      "lat": -64.10183333333333,
+      "lon": -34.666,
+      "alt": 0.0
+    },
+    "date": 1758888417378,
+    "message": "W1QZ-9\u003eT1TU4X,WIDE1-1,WIDE2-1,qAR,W1DMR-4:`c,4l \u001c\u003e1]\"3r}\u003d"
+  },
+  {
+    "latencyMs": 0,
+    "packetsPerHour": 0,
+    "errorCount": 0,
+    "antennaHeightM": 0.0,
+    "powerWatts": 0.0,
+    "digipeater": false,
+    "messagingEnabled": false,
+    "type": "IGATE",
+    "callsign": "KB3PCC-5",
+    "coordinates": {
+      "lat": 39.79233333333333,
+      "lon": -77.36516666666667,
+      "alt": 0.0
+    },
+    "date": 1758900882914,
+    "message": "KB3PCC-5\u003eAPDR17,TCPIP*,qAC,T2FINLAND:\u003d3947.54N/07721.91W)275/002/A\u003d002244 https://aprsdroid.org/South Central PA"
+  },
+  {
+    "latencyMs": 0,
+    "packetsPerHour": 0,
+    "errorCount": 0,
+    "antennaHeightM": 0.0,
+    "powerWatts": 0.0,
+    "digipeater": false,
+    "messagingEnabled": false,
+    "type": "IGATE",
+    "callsign": "IZ0OWE-10",
+    "coordinates": {
+      "lat": 41.914333333333325,
+      "lon": 12.527666666666669,
+      "alt": 0.0
+    },
+    "date": 1758898883096,
+    "message": "IZ0OWE-10\u003eAPADN1,TCPIP*,qAS,SR3ADN:@150122h4154.86N/01231.66EyADN-SYSTEMS-ITALY ADN2224 Italia Roma"
+  }
+]
+;
