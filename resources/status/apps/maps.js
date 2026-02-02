c4654aeeeee95b447d8eb9818edc5381b17e1233
Max Brito <maxbrito@pm.me>
2026-02-02 23:28:56 +0100

    Add Maps app for offline satellite images and maps

---
+++ resources/status/apps/maps.js
@@ -0,0 +1,16 @@
+// Maps app status
+const appMaps = {
+  id: "maps",
+  title: "Maps",
+  description: "Offline satellite images and maps within a 1000 KM radius for navigation without internet",
+  missing: "Custom map layer imports",
+  worksLocally: true,
+  reachableInternet: false,
+  reachableBLE: false,
+  feedback: {
+    like: true,
+    rate: false,
+    comment: true,
+    subscribe: false
+  }
+};
